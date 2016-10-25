9a2bafd11752a4e025b71bdaad36eee50ce26a0a
Artem Garkusha <artem.garkusha@gmail.com>
2016-10-25 21:42:28 +0200

    bottom sheet

--- app/src/main/java/pixel/kotlin/bassblog/ui/MainActivity.kt
+++ app/src/main/java/pixel/kotlin/bassblog/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package pixel.kotlin.bassblog.ui
 
-import android.content.Intent
 import android.database.Cursor
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
+import android.support.design.widget.BottomSheetBehavior.BottomSheetCallback
 import android.support.v4.app.LoaderManager
 import android.support.v4.content.Loader
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import pixel.kotlin.bassblog.PostUtils
 import pixel.kotlin.bassblog.R
@@ -17,6 +19,7 @@ import pixel.kotlin.bassblog.ui.PostAdapter.PostCallback
 class MainActivity : CommunicationActivity(), LoaderManager.LoaderCallbacks<Cursor>, PostCallback {
 
     private var mAdapter: PostAdapter? = null
+    private var mBehavior: BottomSheetBehavior<View>? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,7 @@ class MainActivity : CommunicationActivity(), LoaderManager.LoaderCallbacks<Curs
 
         all_posts_recycler.layoutManager = LinearLayoutManager(applicationContext)
         all_posts_recycler.adapter = mAdapter
+        initBottomSheet()
 
         supportLoaderManager.initLoader(0, Bundle.EMPTY, this)
         NetworkService.start(this)
@@ -45,5 +49,27 @@ class MainActivity : CommunicationActivity(), LoaderManager.LoaderCallbacks<Curs
 
     override fun onPostSelected(blogPost: BlogPost?) {
         MusicPlayerActivity.start(applicationContext, blogPost)
+    }
+
+
+    inner class MyCallback : BottomSheetBehavior.BottomSheetCallback() {
+
+        override fun onSlide(bottomSheet: View, slideOffset: Float) {
+
+        }
+
+        override fun onStateChanged(bottomSheet: View, newState: Int) {
+
+        }
+    }
+
+    private fun initBottomSheet() {
+        val bottomSheet = coordinatorLayout.findViewById(R.id.bottom_sheet)
+        mBehavior = BottomSheetBehavior.from(bottomSheet)
+        mBehavior?.setBottomSheetCallback(MyCallback())
+    }
+
+    public fun toggle(view: View) {
+        mBehavior?.state = if (mBehavior?.state == BottomSheetBehavior.STATE_EXPANDED) BottomSheetBehavior.STATE_COLLAPSED else BottomSheetBehavior.STATE_EXPANDED
     }
 }
