dda88288dfd163163f773f36215148427523e3e9
Artem Garkusha <artem.garkusha@gmail.com>
2016-11-08 22:02:53 +0100

    code clean up

--- app/src/main/java/pixel/kotlin/bassblog/ui/search/SearchFragment.kt
+++ app/src/main/java/pixel/kotlin/bassblog/ui/search/SearchFragment.kt
@@ -1,17 +1,36 @@
 package pixel.kotlin.bassblog.ui.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.search_layout.*
 import pixel.kotlin.bassblog.R
 import pixel.kotlin.bassblog.ui.BinderFragment
 
 
-class SearchFragment : BinderFragment() {
+class SearchFragment : BinderFragment(), TextWatcher {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater!!.inflate(R.layout.all_mix, container, false)
+        return inflater?.inflate(R.layout.search_layout, container, false)
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        search_edit_text.addTextChangedListener(this)
+    }
+
+    override fun afterTextChanged(p0: Editable?) {
+
+    }
+
+    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
+
+    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+    }
 }
